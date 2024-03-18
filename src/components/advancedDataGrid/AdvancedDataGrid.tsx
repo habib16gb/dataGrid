b13c1cbcde72aecb801a3843bb8f1d35d3c133e2
habib16gb <76877928+habib16gb@users.noreply.github.com>
2024-03-18 16:00:39 +0000

    add multiple filter

--- src/components/advancedDataGrid/AdvancedDataGrid.tsx
+++ src/components/advancedDataGrid/AdvancedDataGrid.tsx
@@ -1,27 +1,11 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import InputGrid from "./InputGrid";
 import HeaderGrid from "./HeaderGrid";
-import Select from "../common/Select";
 import { GridColDef } from "../../pages/AdvancedDataGridPage";
 import rows from "../../data/data";
+import FilterTable from "./FilterTable";
 
-enum enOperator {
-  CONTAINS = "contains",
-  EQUALS = "equals",
-  START_WITH = "start with",
-  END_WITH = "end with",
-  IS_EMPTY = "is empty",
-  IS_NOT_EMPTY = "is not empty",
-}
 
-const textOps = [
-  enOperator.CONTAINS,
-  enOperator.START_WITH,
-  enOperator.END_WITH,
-  enOperator.EQUALS,
-];
-
-const numberOps = ["=", ">", "<", "!=", ">=", "<="];
 
 const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 90, type: "number" },
@@ -54,63 +38,25 @@ const columns: GridColDef[] = [
     headerName: "Age",
     width: 100,
     editable: false,
+    type: "number"
   },
   {
     field: "amount",
     headerName: "Amount",
-    width: 100,
+    width: 140,
     editable: true,
+    type: "number"
   },
-];
-
-const filtersFunctions = [
-  {
-    operator: enOperator.CONTAINS,
-    fn: (data: unknown[], field: string, value: string) =>
-      [...data].filter((ele) =>
-        ele[field]?.toLocaleLowerCase()?.includes(value?.toLocaleLowerCase())
-      ),
-  },
-  {
-    operator: enOperator.EQUALS,
-    fn: (data: unknown[], field: string, value: string) =>
-      [...data].filter(
-        (ele) => ele[field]?.toLocaleLowerCase() === value?.toLocaleLowerCase()
-      ),
-  },
-  {
-    operator: enOperator.START_WITH,
-    fn: (data: unknown[], field: string, value: string) =>
-      [...data].filter((ele) =>
-        ele[field]?.toLocaleLowerCase()?.startsWith(value?.toLocaleLowerCase())
-      ),
-  },
-  {
-    operator: enOperator.END_WITH,
-    fn: (data: unknown[], field: string, value: string) =>
-      [...data].filter((ele) =>
-        ele[field]?.toLocaleLowerCase()?.endsWith(value?.toLocaleLowerCase())
-      ),
-  },
-  // {
-  //   operator: enOperator.IS_EMPTY,
-  //   fn: (data: unknown[], field: string) =>
-  //     [...data].filter((ele) => ele[field].trim().length === 0),
-  // },
-  // {
-  //   operator: enOperator.IS_NOT_EMPTY,
-  //   fn: (data: unknown[], field: string) =>
-  //     [...data].filter((ele) => ele[field].trim().length !== 0),
-  // },
 ];
 
 const AdvancedDataGrid = () => {
   const [data, setData] = useState(rows);
-  const [selectedField, setSelectedField] = useState("");
-  const [type, setType] = useState("");
-  const [selectedFilter, setSelectedFilter] = useState("");
-
-  const filterValueRef = useRef(null);
+  const [filterData, setFilterData] = useState(data)
+ 
+  useEffect(() => {
+    setFilterData(data)
+  },[data])
+ 
 
   const handleBlur = (e, index: number, field: string) =>
     setData((prev) => {
@@ -119,56 +65,14 @@ const AdvancedDataGrid = () => {
       return newData;
     });
 
-  useEffect(() => {
-    const ele = columns.find((col) => col.field === selectedField);
-    setType(ele?.type || "text");
-  }, [selectedField]);
-
-  const handleFilter = (e) => {
-    const operation = filtersFunctions.find(
-      (fn) => fn.operator === selectedFilter
-    );
-    // setData(
-    //   rows.filter((row) =>
-    //     row[selectedField]
-    //       ?.toLocaleLowerCase()
-    //       .includes(e.target.value.toLocaleLowerCase())
-    //   )
-    // );
-    console.log(selectedField, selectedFilter, filterValueRef.current.value);
-
-    setData(operation?.fn(rows, selectedField, filterValueRef.current.value));
-  };
-
+    console.log('data',data)
+    console.log('filterdata',filterData)
   return (
     <div>
-      <div className='flex items-center gap-2 w-1/2 m-4'>
-        <Select
-          selectedOption={selectedField}
-          setSelectedOption={setSelectedField}
-          options={columns.map(({ field }) => field)}
-        />
-        {type === "number" && (
-          <Select
-            selectedOption={selectedFilter}
-            setSelectedOption={setSelectedFilter}
-            options={numberOps}
-          />
-        )}
-        {type === "text" && (
-          <Select
-            selectedOption={selectedFilter}
-            setSelectedOption={setSelectedFilter}
-            options={textOps}
-          />
-        )}
-
-        <input
-          className='shadow px-4 py-2 rounded-md'
-          type='text'
-          onChange={handleFilter}
-          ref={filterValueRef}
-        />
+      <div>
+        <FilterTable columns={columns} setFilterData={setData} data={rows} />
+        <FilterTable columns={columns} setFilterData={setFilterData} data={data}  />
+        <FilterTable columns={columns} setFilterData={setFilterData} data={data}  />
       </div>
       <table>
         <thead>
@@ -181,7 +85,7 @@ const AdvancedDataGrid = () => {
           </tr>
         </thead>
         <tbody>
-          {data.map((row, index) => (
+          {filterData.map((row, index) => (
             <tr key={row.id}>
               {columns.map((col) => (
                 <td key={col.field}>
